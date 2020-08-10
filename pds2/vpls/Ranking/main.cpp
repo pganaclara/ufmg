32a15267c5db158d0575b0b023787d7b6d870568
Ana <pganaclara@gmail.com>
2020-08-09 21:21:57 -0300

    ranking correto

--- pds2/vpls/Ranking/main.cpp
+++ pds2/vpls/Ranking/main.cpp
@@ -3,31 +3,45 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
-#include <list>
+#include <vector>
+#include <algorithm>
 
 int main(){
   std::string nome;
   int matricula;
   float notas[5];
-  std::list<Estudante> listaEstudantes;
+  std::vector<Estudante> listaEstudantes;
   Estudante estudante;
 
-  for (int j = 1; j <= 2; j++){
+  for (int j = 1; j <= 10; j++){
     std::cin >> estudante.matricula >> estudante.nome;
-    Estudante* ej = new Estudante;
+    Estudante* temp = new Estudante;
 
     for (int i = 0; i < 5; i++){
       std::cin >> estudante.notas[i];
-      ej->notas[i] = estudante.notas[i];
+      temp->notas[i] = estudante.notas[i];
     }
 
     std::cout << std::fixed << std::showpoint; 
     std::cout << std::setprecision(2);
 
-    estudante.notas[0] = ej->calcularRSG();
+    estudante.notas[0] = temp->calcularRSG();
 
     listaEstudantes.push_back(estudante);
   }
 
+    std::sort( listaEstudantes.begin(), listaEstudantes.end(), 
+    []( const Estudante& a, const Estudante&b ){
+        return a.notas[0] > b.notas[0];
+    } );
+    
+    std::vector<Estudante>ranking;
+    ranking = std::vector<Estudante>(listaEstudantes.begin(), listaEstudantes.begin() + 3);
+
+    for (auto const &a : ranking){
+        std::cout << a.matricula << " " << a.nome << " " << a.notas[0]  << std::endl;
+    }
+    
+    
   return 0;
 }
