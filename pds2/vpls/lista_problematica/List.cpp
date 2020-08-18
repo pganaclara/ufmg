ef94e52001b21e73cbc4da8287f0cf605c0ba37c
Ana <pganaclara@gmail.com>
2020-08-18 15:26:14 -0300

    lista

---
+++ pds2/vpls/lista_problematica/List.cpp
@@ -0,0 +1,101 @@
+#include "List.hpp"
+
+List::List(){
+	_size = 0;
+	_head = nullptr;
+}
+
+void List::insert(int value){
+  Node* node = new Node();
+	node->_value = value;
+	node->_next = nullptr;
+  if(_head == nullptr){
+    _head = node;
+    _toe = node;
+  } else {
+    _toe->_next = node;
+    _toe = node;
+  }
+	_size++;
+}
+
+void List::clearList() {
+	_head = NULL;
+}
+
+void List::print(){
+	Node* head = _head;
+	if (_size > 0){
+		while(head){
+			std::cout << head->_value << " ";
+			head = head->_next;
+		}
+		std::cout<<std::endl;
+	}
+	else{
+	    std::cout<< "-" <<std::endl;
+		return;
+	}
+}
+
+int List::removeFirst(){
+  if (_size != 0){
+    _size--;
+    Node* current = _head;
+	  _head = _head->_next;
+    delete current;
+	  return current->_value;
+  } else {
+    return 1;
+  }
+}
+
+int List::indexOf(int value){
+	if (_size > 0){
+		Node* head = _head;
+		int index = 0;
+		while(head && head->_value != value){
+			index++;
+			head = head->_next;
+		}
+		if (head->_value == value)
+			return index; 
+		return 0;
+	}
+	else{
+		return 0;
+	}
+}
+
+void List::remove(int value){
+	if(_size == 0){
+		return;
+	}
+	else if(_head->_value == value){
+		Node* current = _head;
+		_head = _head->_next;
+    _size--;
+		delete current;
+	}
+	else{
+		Node* previous = _head;
+		Node* current = _head->_next;
+		while(current != NULL) {
+			if(current->_value == value) {
+				break;
+			}
+			else {
+				previous = current;
+				current = current->_next;
+			}
+		}
+		if(current == NULL) {
+			return;
+		}
+		else {
+			previous->_next = current->_next;
+      _size--;
+			delete current;
+		}
+	}
+}
---
+++ pds2/vpls/lista_problematica/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "List.hpp"
+
+int main(){
+  int N, K;
+
+  std::cin >> N >> K;
+  List *L = new List();
+    
+  for (int i = N; i >= 0; i--){
+    L->insert(i); //insere um a um os elementos [N, 0]
+  }
+
+  L->print();
+  std::cout << L->_size << std::endl;
+
+  for (int j = 0; j < K; j++){
+    L->removeFirst(); // remove os primeiros elementos k vezes
+  }
+
+  L->print();
+  std::cout << L->_size << std::endl;
+
+  for (int k = 0; k <= N; k+=2) {
+    L->remove(k); // começa retirando o 0 e vai de 2 em 2, tirando os pares
+  }
+
+  L->print();
+  std::cout << L->_size << std::endl;
+
+  delete L;
+	return 0;
+}
